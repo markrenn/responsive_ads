a63e3c8220dad856cf18eaaa8a0a3313a22d06af
markrenn <markrenn@gmail.com>
2012-12-03 16:31:38 +0800

    first commit

---
+++ ads/rad.php
@@ -0,0 +1,70 @@
+<html>
+<body>
+<?php
+switch ($_GET['r']) {
+    case 'button_ad':
+?>
+<a href=""><img src="img/button_125x12.png" width="125" height="125" alt="button ad"></a>
+<?php
+        break;
+    case 'srec_ad':
+?>
+<a href=""><img src="img/srec_180x150_en.png" width="180" height="150" alt="srec ad"></a>
+<?php
+        break;
+    case 'ssquare_ad':
+?>
+<a href=""><img src="img/ssquare_200x200_en.jpg" width="200" height="200" alt="ssquare ad"></a>
+<?php
+        break;
+    case 'square_ad':
+?>
+<a href=""><img src="img/square_250x250_en.jpg" width="250" height="250" alt="square ad"></a>
+<?php
+        break;
+     case 'mrec_ad':
+?>
+<script>
+<!-- AdSpace AsiaOne Homepage -->
+document.write('<script type="text/javascript" src="http://ad.doubleclick.net/adj/StraitsTimes/BN_Singapore;pos=1;adtype=1;adtype=2;adtype=3;adtype=4;adtype=5;tile=5;sz=300x250;ord=?"><\/script>');
+<!-- /AdSpace AsiaOne Homepage -->
+</script>
+<?php
+        break;
+   case 'lrec_ad': 
+?>
+<a href=""><img src="img/lrec_336x280_en.jpg" width="336" height="280" alt="square ad"></a>
+<?php
+        break;
+    case 'sph_lrec_ad': // non-standard 350x200
+?>
+<script>
+<!-- AdSpace AsiaOne Homepage -->
+document.write('<script type="text/javascript" src="http://ad.doubleclick.net/adj/StraitsTimes/HomePage;pos=1;adtype=1;adtype=2;adtype=3;adtype=4; adtype=5;tile=5;sz=350x200;ord=?"><\/script>');
+<!-- /AdSpace AsiaOne Homepage -->
+</script>
+<?php
+        break;
+    case 'halfbanner_ad':
+?>
+<a href=""><img src="img/halfbanner_234x60_en.png" width="234" height="60" alt="halfbanner ad"></a>
+<?php
+        break;
+    case 'banner_ad':
+?>
+<a href=""><img src="img/banner_468x60_en.jpg" width="468" height="60" alt="banner ad"></a>
+<?php
+        break;
+    case 'leaderboard_ad':
+?>
+<script>
+<!-- AdSpace AsiaOne Homepage -->
+document.write('<script type="text/javascript" src="http://ad.doubleclick.net/adj/AsiaOneHomePage/;ad=sync;pos=1;adtype=1;adtype=2;adtype=3;adtype=4;adtype=5;tile=3;dcopt=ist;sz=728x90;ord=?"><\/script>');
+<!-- /AdSpace AsiaOne Homepage -->
+</script>
+<?php
+        break;
+}
+?>
+</body>
+</html>
